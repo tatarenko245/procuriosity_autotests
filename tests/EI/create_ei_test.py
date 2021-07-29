a0645a8960a3369eb4b9da205a516c17de9da2c1
roman <tatarenko.roman@gmail.com>
2021-07-29 15:55:41 +0300

    29/07/2021 15:55 Summary: 1. Refactor Allure Report.

--- tests/EI/create_ei_test.py
+++ tests/EI/create_ei_test.py
@@ -121,6 +121,7 @@ class TestCreateEi:
         with allure.step('# 2. Take EI payload based on full data model'):
             payload = copy.deepcopy(EiPayload())
             GlobalClassCreateEi.payload_for_create_ei = payload.add_optionals_fields()
+            del GlobalClassCreateEi.payload_for_create_ei['tender']
             allure.attach(str(GlobalClassCreateEi.payload_for_create_ei), 'Payload')
 
         with allure.step('# 3. Send request to create EI'):
@@ -189,19 +190,25 @@ class TestCreateEi:
                 )
         # ============================
 
+    # @allure.title('Check EI release after Ei creation on model without optional fields')
+    # def test_check_ei_release_after_ei_creation_on_model_without_optional_fields(self, environment, country, language,
+    #                                                                              cassandra_username,
+    #                                                                              cassandra_password):
     #     GlobalClassCreateEi.country = country
     #     GlobalClassCreateEi.language = language
     #     GlobalClassCreateEi.cassandra_username = cassandra_username
     #     GlobalClassCreateEi.cassandra_password = cassandra_password
     #     GlobalClassCreateEi.environment = environment
     #     GlobalClassCreateEi.hosts = Environment().choose_environment(GlobalClassCreateEi.environment)
-    #     host_for_bpe = GlobalClassCreateEi.hosts[1]
+    #     GlobalClassCreateEi.host_for_bpe = GlobalClassCreateEi.hosts[1]
     #     GlobalClassCreateEi.host_for_service = GlobalClassCreateEi.hosts[2]
     #     GlobalClassCreateEi.cassandra_cluster = GlobalClassCreateEi.hosts[0]
-    #     access_token = PlatformAuthorization(host_for_bpe).get_access_token_for_platform_one()
-    #     GlobalClassCreateEi.operation_id = PlatformAuthorization(host_for_bpe).get_x_operation_id(access_token)
-    #     payload = copy.deepcopy(EiPayload())
-    #     GlobalClassCreateEi.payload_for_create_ei = payload.add_optionals_fields()
+    #     GlobalClassCreateEi.access_token = PlatformAuthorization(
+    #         GlobalClassCreateEi.host_for_bpe).get_access_token_for_platform_one()
+    #     GlobalClassCreateEi.operation_id = PlatformAuthorization(
+    #         GlobalClassCreateEi.host_for_bpe).get_x_operation_id(
+    #         GlobalClassCreateEi.access_token)
+    #     GlobalClassCreateEi.payload_for_create_ei = copy.deepcopy(EiPayload().obligatory_model_of_payload())
     #     allure.attach(str(GlobalClassCreateEi.payload_for_create_ei), 'Payload')
     #
     # @allure.step('Send request to create EI')
@@ -220,7 +227,7 @@ class TestCreateEi:
     #     GlobalClassCreateEi.message = KafkaMessage(GlobalClassCreateEi.operation_id).get_message_from_kafka()
     #     actual_ei_release_model = requests.get(url=f"{GlobalClassCreateEi.message['data']['url']}/"
     #                                                f"{GlobalClassCreateEi.message['data']['ocid']}").json()
-    #     release = EiRelease(
+    #     expected_ei_release_model = EiRelease(
     #         operation_date=GlobalClassCreateEi.message['data']['operationDate'],
     #         release_id=actual_ei_release_model['releases'][0]['id'],
     #         tender_id=actual_ei_release_model['releases'][0]['tender']['id'],
@@ -228,10 +235,7 @@ class TestCreateEi:
     #         environment=GlobalClassCreateEi.environment,
     #         payload_for_create_ei=GlobalClassCreateEi.payload_for_create_ei,
     #         language=GlobalClassCreateEi.language
-    #     )
-    #     release.full_data_data_model()
-    #     expected_ei_release_model = release.add_tender_with_items_array(
-    #         actual_items_array=actual_ei_release_model['releases'][0]['tender']['items'])
+    #     ).obligatory_data_model()
     #     allure.attach(str(actual_ei_release_model), "Actual Ei release")
     #     allure.attach(str(expected_ei_release_model), "Expected Ei release")
     #     compare_releases = DeepDiff(actual_ei_release_model, expected_ei_release_model)
@@ -259,80 +263,6 @@ class TestCreateEi:
     #     except ValueError:
     #         print("Check the message in kafka topic")
 
-#
-# @allure.feature('Check EI release after Ei creation on model without optional fields')
-# class TestCheckEiReleaseAfterEiCreationOnModelWithoutOptionalFields:
-#     @allure.step('Take EI payload based on full data model')
-#     def test_setup(self, environment, country, language, cassandra_username, cassandra_password):
-#         GlobalClassCreateEi.country = country
-#         GlobalClassCreateEi.language = language
-#         GlobalClassCreateEi.cassandra_username = cassandra_username
-#         GlobalClassCreateEi.cassandra_password = cassandra_password
-#         GlobalClassCreateEi.environment = environment
-#         GlobalClassCreateEi.hosts = Environment().choose_environment(GlobalClassCreateEi.environment)
-#         GlobalClassCreateEi.host_for_bpe = GlobalClassCreateEi.hosts[1]
-#         GlobalClassCreateEi.host_for_service = GlobalClassCreateEi.hosts[2]
-#         GlobalClassCreateEi.cassandra_cluster = GlobalClassCreateEi.hosts[0]
-#         GlobalClassCreateEi.access_token = PlatformAuthorization(
-#             GlobalClassCreateEi.host_for_bpe).get_access_token_for_platform_one()
-#         GlobalClassCreateEi.operation_id = PlatformAuthorization(
-#             GlobalClassCreateEi.host_for_bpe).get_x_operation_id(
-#             GlobalClassCreateEi.access_token)
-#         GlobalClassCreateEi.payload_for_create_ei = copy.deepcopy(EiPayload().obligatory_model_of_payload())
-#         allure.attach(str(GlobalClassCreateEi.payload_for_create_ei), 'Payload')
-#
-#     @allure.step('Send request to create EI')
-#     def test_check_status_code_of_request(self):
-#         GlobalClassCreateEi.send_the_request_create_ei = Requests().create_ei(
-#             host_of_request=GlobalClassCreateEi.host_for_bpe,
-#             access_token=GlobalClassCreateEi.access_token,
-#             x_operation_id=GlobalClassCreateEi.operation_id,
-#             country=GlobalClassCreateEi.country,
-#             language=GlobalClassCreateEi.language,
-#             payload=GlobalClassCreateEi.payload_for_create_ei
-#         )
-#
-#     @allure.step('See result')
-#     def test_check_ei_release_in_public_point(self):
-#         GlobalClassCreateEi.message = KafkaMessage(GlobalClassCreateEi.operation_id).get_message_from_kafka()
-#         actual_ei_release_model = requests.get(url=f"{GlobalClassCreateEi.message['data']['url']}/"
-#                                                    f"{GlobalClassCreateEi.message['data']['ocid']}").json()
-#         expected_ei_release_model = EiRelease(
-#             operation_date=GlobalClassCreateEi.message['data']['operationDate'],
-#             release_id=actual_ei_release_model['releases'][0]['id'],
-#             tender_id=actual_ei_release_model['releases'][0]['tender']['id'],
-#             ei_id=GlobalClassCreateEi.message['data']['ocid'],
-#             environment=GlobalClassCreateEi.environment,
-#             payload_for_create_ei=GlobalClassCreateEi.payload_for_create_ei,
-#             language=GlobalClassCreateEi.language
-#         ).obligatory_data_model()
-#         allure.attach(str(actual_ei_release_model), "Actual Ei release")
-#         allure.attach(str(expected_ei_release_model), "Expected Ei release")
-#         compare_releases = DeepDiff(actual_ei_release_model, expected_ei_release_model)
-#         assert compare_actual_result_and_expected_result(
-#             expected_result=str({}),
-#             actual_result=str(compare_releases)
-#         )
-#
-#     def test_teardown(self):
-#         try:
-#             if GlobalClassCreateEi.check_message is True:
-#                 database = CassandraSession(
-#                     cassandra_username=GlobalClassCreateEi.cassandra_username,
-#                     cassandra_password=GlobalClassCreateEi.cassandra_password,
-#                     cassandra_cluster=GlobalClassCreateEi.cassandra_cluster
-#                 )
-#                 database.create_ei_process_cleanup_table_of_services(
-#                     ei_id=GlobalClassCreateEi.message['data']['ocid']
-#                 )
-#                 database.cleanup_steps_of_process(
-#                     operation_id=GlobalClassCreateEi.operation_id
-#                 )
-#             else:
-#                 pass
-#         except ValueError:
-#             print("Check the message in kafka topic")
-#
 #
 # @allure.feature('Check EI release data after Ei creation based on full data model with 3 items objects')
 # class TestCheckEiReleaseDataAfterEiCreationBasedOnFullDataModelWith3ItemsObjects:
