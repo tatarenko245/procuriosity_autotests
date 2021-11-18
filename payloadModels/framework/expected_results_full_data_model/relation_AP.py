1a419c9ddca7add67c0149e539a575d10a27cadf
kate <katyakhaletska@gmail.com>
2021-11-18 12:27:15 +0200

    Add relation AP expected result

---
+++ payloadModels/framework/expected_results_full_data_model/relation_AP.py
@@ -0,0 +1,238 @@
+  relation_ap_pn = {
+  "ocid": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154",
+  "compiledRelease": {
+    "ocid": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154",
+    "id": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154-1637230994563",
+    "date": "2021-11-18T10:23:14Z",
+    "tag": [
+      "planning" ],
+    "initiationType": "tender",
+    "language": "ro",
+    "tender": {
+      "id": "4a76eb09-4d00-487c-943e-e7b78e2e28b1",
+      "title": "Planning Notice",
+      "description": "Contracting process is planned",
+      "status": "planning",
+      "statusDetails": "aggregated",
+      "lotGroups": [
+        {
+          "optionToCombine": false
+        } ],
+      "tenderPeriod": {
+        "startDate": "2020-12-01T00:07:00Z"
+      },
+      "hasEnquiries": false,
+      "documents": [
+        {
+          "id": "92f6d26c-cd55-4192-9a2a-798d934c6fc9-1633002324899",
+          "documentType": "evaluationCriteria",
+          "title": "document title",
+          "description": "document description",
+          "url": "https://dev.bpe.eprocurement.systems/api/v1/storage/get/92f6d26c-cd55-4192-9a2a-798d934c6fc9-1633002324899",
+          "datePublished": "2021-09-30T11:54:20Z"
+        } ],
+      "submissionMethod": [
+        "electronicSubmission" ],
+      "submissionMethodDetails": "Lista platformelor: achizitii, ebs, licitatie, yptender",
+      "submissionMethodRationale": [
+        "Ofertele vor fi primite prin intermediul unei platforme electronice de achiziții publice" ],
+      "requiresElectronicCatalogue": false
+    },
+    "hasPreviousNotice": false,
+    "purposeOfNotice": {
+      "isACallForCompetition": false
+    },
+    "relatedProcesses": [
+      {
+        "id": "12d391e3-4858-11ec-b310-c7ecf598564f",
+        "relationship": [
+          "parent" ],
+        "scheme": "ocid",
+        "identifier": "ocds-t1s2t3-MD-1637230365154",
+        "uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230365154/ocds-t1s2t3-MD-1637230365154"
+      },
+      {
+        "id": "0bd218c9-65e8-4f10-916d-c9f62a40ff7d",
+        "relationship": [
+          "framework" ],
+        "scheme": "ocid",
+        "identifier": "ocds-t1s2t3-MD-1637230371725",
+        "uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230371725/ocds-t1s2t3-MD-1637230371725"
+      } ]
+  }
+}
+
+  relation_ap_ap = {
+  "ocid": "ocds-t1s2t3-MD-1637230371725-AP-1637230371725",
+  "compiledRelease": {
+    "ocid": "ocds-t1s2t3-MD-1637230371725-AP-1637230371725",
+    "id": "ocds-t1s2t3-MD-1637230371725-AP-1637230371725-1637230994570",
+    "date": "2021-11-18T10:23:14Z",
+    "tag": [
+      "planning" ],
+    "initiationType": "tender",
+    "language": "ro",
+    "parties": [
+      {
+        "id": "MD-IDNO-3",
+        "name": "name of PE from AP (future cpb)",
+        "identifier": {
+          "scheme": "MD-IDNO",
+          "id": "3",
+          "legalName": "identifier/legal name",
+          "uri": "ident uri"
+        },
+        "address": {
+          "streetAddress": "street address",
+          "postalCode": "11",
+          "addressDetails": {
+            "country": {
+              "scheme": "iso-alpha2",
+              "id": "MD",
+              "description": "Moldova, Republica",
+              "uri": "https://www.iso.org"
+            },
+            "region": {
+              "scheme": "CUATM",
+              "id": "0101000",
+              "description": "mun.Chişinău",
+              "uri": "http://statistica.md"
+            },
+            "locality": {
+              "scheme": "other",
+              "id": "localityid",
+              "description": "locality/description"
+            }
+          }
+        },
+        "additionalIdentifiers": [
+          {
+            "scheme": "md-idno",
+            "id": "445521",
+            "legalName": "legalName",
+            "uri": "uri"
+          } ],
+        "contactPoint": {
+          "name": "contactPoint/name",
+          "email": "contactPoint/email",
+          "telephone": "contactPoint/456-95-96",
+          "faxNumber": "fax-number",
+          "url": "CP url"
+        },
+        "roles": [
+          "centralPurchasingBody" ]
+      },
+      {
+        "id": "MD-IDNO-1",
+        "name": "Buyer name",
+        "identifier": {
+          "scheme": "MD-IDNO",
+          "id": "1",
+          "legalName": "legal_name",
+          "uri": "uri"
+        },
+        "address": {
+          "streetAddress": "street address",
+          "postalCode": "postalCode",
+          "addressDetails": {
+            "country": {
+              "scheme": "iso-alpha2",
+              "id": "MD",
+              "description": "Moldova, Republica",
+              "uri": "https://www.iso.org"
+            },
+            "region": {
+              "scheme": "CUATM",
+              "id": "0101000",
+              "description": "mun.Chişinău",
+              "uri": "http://statistica.md"
+            },
+            "locality": {
+              "scheme": "CUATM",
+              "id": "0101000",
+              "description": "mun.Chişinău",
+              "uri": "http://statistica.md"
+            }
+          }
+        },
+        "additionalIdentifiers": [
+          {
+            "scheme": "md-idno",
+            "id": "445521",
+            "legalName": "legalName",
+            "uri": "uri"
+          } ],
+        "contactPoint": {
+          "name": "contactPoint/name",
+          "email": "contactPoint/email",
+          "telephone": "contactPoint/456-95-96",
+          "faxNumber": "fax-number",
+          "url": "url"
+        },
+        "details": {
+          "typeOfBuyer": "MINISTRY",
+          "mainGeneralActivity": "HEALTH",
+          "mainSectoralActivity": "WATER"
+        },
+        "roles": [
+          "client" ]
+      } ],
+    "tender": {
+      "id": "bc021cb4-6097-4b7f-8783-f5f9dc68bae9",
+      "title": "tender.title AP",
+      "description": "tender description AP",
+      "status": "planning",
+      "statusDetails": "aggregation",
+      "tenderPeriod": {
+        "startDate": "2020-12-01T11:07:00Z"
+      },
+      "hasEnquiries": false,
+      "documents": [
+        {
+          "id": "92f6d26c-cd55-4192-9a2a-798d934c6fc9-1633002324899",
+          "documentType": "evaluationCriteria",
+          "title": "document title",
+          "description": "document description",
+          "url": "https://dev.bpe.eprocurement.systems/api/v1/storage/get/92f6d26c-cd55-4192-9a2a-798d934c6fc9-1633002324899",
+          "datePublished": "2021-09-30T11:54:20Z"
+        } ],
+      "submissionMethod": [
+        "electronicSubmission" ],
+      "submissionMethodDetails": "Lista platformelor: achizitii, ebs, licitatie, yptender",
+      "submissionMethodRationale": [
+        "Ofertele vor fi primite prin intermediul unei platforme electronice de achiziții publice" ],
+      "requiresElectronicCatalogue": false,
+      "value": {
+        "amount": 90.00,
+        "currency": "EUR"
+      },
+      "procurementMethodRationale": "tender/procurementMethodRationale AP",
+      "classification": {
+        "scheme": "CPV",
+        "id": "50100000-6",
+        "description": "Servicii de reparare şi de întreţinere a vehiculelor şi a echipamentelor aferente şi servicii conexe"
+      }
+    },
+    "hasPreviousNotice": false,
+    "purposeOfNotice": {
+      "isACallForCompetition": false
+    },
+    "relatedProcesses": [
+      {
+        "id": "16bcda01-4858-11ec-b310-c7ecf598564f",
+        "relationship": [
+          "parent" ],
+        "scheme": "ocid",
+        "identifier": "ocds-t1s2t3-MD-1637230371725",
+        "uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230371725/ocds-t1s2t3-MD-1637230371725"
+      },
+      {
+        "id": "02cb9155-0ad1-4d19-bd04-a3fda60b0637",
+        "relationship": [
+          "x_scope" ],
+        "scheme": "ocid",
+        "identifier": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154",
+        "uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230365154/ocds-t1s2t3-MD-1637230365154-PN-1637230365154"
+      } ]
+  }
+}
