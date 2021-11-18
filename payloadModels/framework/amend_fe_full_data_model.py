c0f28f768f4e6ba2798f1091ea04848288653934
kate <katyakhaletska@gmail.com>
2021-11-18 14:33:34 +0200

    Add amend FE payload

---
+++ payloadModels/framework/amend_fe_full_data_model.py
@@ -0,0 +1,49 @@
+  amend_fe_full_data_model = {
+  "preQualification": {
+    "period": {
+      "endDate": "2021-12-24T18:54:00Z"
+    }
+  },
+  "tender": {
+    "title": "updateFE: tender.title",
+    "description": "updateFE: tender.description",
+
+    "procurementMethodRationale": "updateFE:procurementMethodRationale",
+    "procuringEntity": {
+      "id": "MD-IDNO-3",
+      "persones": [
+        {
+          "title": "updateFE.persones",
+          "name": "name",
+          "identifier": {
+            "scheme": "string",
+            "id": "stringnew",
+            "uri": "string"
+          },
+          "businessFunctions": [
+            {
+              "id": "businessFunctions1",
+              "type": "chairman",
+              "jobTitle": "string",
+              "period": {
+                "startDate": "2020-09-22T14:38:20Z"
+              },
+              "documents": [
+                {
+                  "id": "{{doc}}",
+                  "documentType": "regulatoryDocument",
+                  "title": "string",
+                  "description": "string"
+                }]
+            }]
+        }]
+    },
+    "documents": [
+      {
+        "documentType": "evaluationCriteria",
+        "id": "{{doc}}",
+        "title": "doctitle`",
+        "description": "docdesc`"
+      }]
+  }
+}
