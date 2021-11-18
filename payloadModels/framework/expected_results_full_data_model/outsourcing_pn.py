3e85fc01bd3634c271e00591ece90d4e4af36024
kate <katyakhaletska@gmail.com>
2021-11-18 12:18:08 +0200

    Add outsourcing PN expected result

---
+++ payloadModels/framework/expected_results_full_data_model/outsourcing_pn.py
@@ -0,0 +1,127 @@
+  outsourcing_pn_pn = {
+ 	"ocid": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154",
+ 	"compiledRelease": {
+ 		"ocid": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154",
+ 		"id": "ocds-t1s2t3-MD-1637230365154-PN-1637230365154-1637230439152",
+ 		"date": "2021-11-18T10:13:58Z",
+ 		"tag": ["planning"],
+ 		"initiationType": "tender",
+ 		"language": "ro",
+ 		"tender": {
+ 			"id": "4a76eb09-4d00-487c-943e-e7b78e2e28b1",
+ 			"title": "Planning Notice",
+ 			"description": "Contracting process is planned",
+ 			"status": "planning",
+ 			"statusDetails": "aggregationPending",
+ 			"lotGroups": [{
+ 				"optionToCombine": false
+ 			}],
+ 			"tenderPeriod": {
+ 				"startDate": "2020-12-01T00:07:00Z"
+ 			},
+ 			"hasEnquiries": false,
+ 			"documents": [{
+ 				"id": "92f6d26c-cd55-4192-9a2a-798d934c6fc9-1633002324899",
+ 				"documentType": "evaluationCriteria",
+ 				"title": "document title",
+ 				"description": "document description",
+ 				"url": "https://dev.bpe.eprocurement.systems/api/v1/storage/get/92f6d26c-cd55-4192-9a2a-798d934c6fc9-1633002324899",
+ 				"datePublished": "2021-09-30T11:54:20Z"
+ 			}],
+ 			"submissionMethod": ["electronicSubmission"],
+ 			"submissionMethodDetails": "Lista platformelor: achizitii, ebs, licitatie, yptender",
+ 			"submissionMethodRationale": ["Ofertele vor fi primite prin intermediul unei platforme electronice de achiziții publice"],
+ 			"requiresElectronicCatalogue": false
+ 		},
+ 		"hasPreviousNotice": false,
+ 		"purposeOfNotice": {
+ 			"isACallForCompetition": false
+ 		},
+ 		"relatedProcesses": [{
+ 			"id": "12d391e3-4858-11ec-b310-c7ecf598564f",
+ 			"relationship": ["parent"],
+ 			"scheme": "ocid",
+ 			"identifier": "ocds-t1s2t3-MD-1637230365154",
+ 			"uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230365154/ocds-t1s2t3-MD-1637230365154"
+ 		}, {
+ 			"id": "0bd218c9-65e8-4f10-916d-c9f62a40ff7d",
+ 			"relationship": ["framework"],
+ 			"scheme": "ocid",
+ 			"identifier": "ocds-t1s2t3-MD-1637230371725",
+ 			"uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230371725/ocds-t1s2t3-MD-1637230371725"
+ 		}]
+ 	}
+ }
+
+  outsourcing_pn_fa = {
+ 	"ocid": "ocds-t1s2t3-MD-1637230371725",
+ 	"compiledRelease": {
+ 		"ocid": "ocds-t1s2t3-MD-1637230371725",
+ 		"id": "ocds-t1s2t3-MD-1637230371725-1637230439158",
+ 		"date": "2021-11-18T10:13:58Z",
+ 		"tag": ["compiled"],
+ 		"initiationType": "tender",
+ 		"language": "ro",
+ 		"tender": {
+ 			"id": "bc021cb4-6097-4b7f-8783-f5f9dc68bae9",
+ 			"title": "tender.title AP",
+ 			"description": "tender description AP",
+ 			"status": "planning",
+ 			"statusDetails": "aggregatePlanning",
+ 			"hasEnquiries": false,
+ 			"value": {
+ 				"currency": "EUR"
+ 			},
+ 			"procurementMethod": "selective",
+ 			"procurementMethodDetails": "testClosedFA",
+ 			"procurementMethodRationale": "tender/procurementMethodRationale AP",
+ 			"eligibilityCriteria": "Regulile generale privind naționalitatea și originea, precum și alte criterii de eligibilitate sunt enumerate în Ghidul practic privind procedurile de contractare a acțiunilor externe ale UE (PRAG)",
+ 			"contractPeriod": {
+ 				"startDate": "2021-12-10T09:30:00Z",
+ 				"endDate": "2021-12-11T10:30:00Z"
+ 			},
+ 			"acceleratedProcedure": {
+ 				"isAcceleratedProcedure": false
+ 			},
+ 			"classification": {
+ 				"scheme": "CPV",
+ 				"id": "50100000-6",
+ 				"description": "Servicii de reparare şi de întreţinere a vehiculelor şi a echipamentelor aferente şi servicii conexe"
+ 			},
+ 			"designContest": {
+ 				"serviceContractAward": false
+ 			},
+ 			"electronicWorkflows": {
+ 				"useOrdering": false,
+ 				"usePayment": false,
+ 				"acceptInvoicing": false
+ 			},
+ 			"jointProcurement": {
+ 				"isJointProcurement": false
+ 			},
+ 			"legalBasis": "DIRECTIVE_2009_81_EC",
+ 			"procedureOutsourcing": {
+ 				"procedureOutsourced": false
+ 			},
+ 			"dynamicPurchasingSystem": {
+ 				"hasDynamicPurchasingSystem": false
+ 			},
+ 			"framework": {
+ 				"isAFramework": true
+ 			}
+ 		},
+ 		"relatedProcesses": [{
+ 			"id": "16bcda00-4858-11ec-b310-c7ecf598564f",
+ 			"relationship": ["aggregatePlanning"],
+ 			"scheme": "ocid",
+ 			"identifier": "ocds-t1s2t3-MD-1637230371725-AP-1637230371725",
+ 			"uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230371725/ocds-t1s2t3-MD-1637230371725-AP-1637230371725"
+ 		}, {
+ 			"id": "cbc8a326-708d-4e33-8683-2d7d40e4c909",
+ 			"relationship": ["x_demand"],
+ 			"scheme": "ocid",
+ 			"identifier": "ocds-t1s2t3-MD-1637230365154",
+ 			"uri": "http://dev.public.eprocurement.systems/tenders/ocds-t1s2t3-MD-1637230365154/ocds-t1s2t3-MD-1637230365154"
+ 		}]
+ 	}
+ }
