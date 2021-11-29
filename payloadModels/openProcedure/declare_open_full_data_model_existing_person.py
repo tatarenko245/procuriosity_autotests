b5d3815bd4d4eb283d0f7d4bc8b437dfc7ac405c
kate <katyakhaletska@gmail.com>
2021-11-29 14:54:46 +0200

    add payload declare_open_full_data_model_existing_person

---
+++ payloadModels/openProcedure/declare_open_full_data_model_existing_person.py
@@ -0,0 +1,40 @@
+declare_open_full_data_model_existing_person = {
+
+  "requirementResponse": {
+    "id": "{{operation-id}}",
+    "value": true,
+    "relatedTenderer": {
+      "id": "MD-IDNO-888ee7e7-888e-4ea4-924a-51a4fbb7d68f"
+    },
+    "responder": {
+      "title": "Ms.",
+      "name": "createCN: persones name",
+      "identifier": {
+        "scheme": "MD-IDNO",
+        "id": "555555",
+        "uri": "uri"
+      },
+      "businessFunctions": [
+        {
+          "id": "businessFunction",
+          "type": "priceOpener",
+          "jobTitle": "Chief Executive Officer",
+          "period": {
+            "startDate": "2019-10-29T15:20:10Z"
+          },
+          ".documents": [
+            {
+              "id": "9004ab09-ca9b-4cb7-95f4-525a3bf64734-1574862409148",
+              "documentType": "regulatoryDocument",
+              "title": "string",
+              "description": "string"
+            }
+          ]
+        }
+      ]
+    },
+    "requirement": {
+      "id": "1b99ef92-7d79-400b-8eef-f3ffd54a3fe7"
+    }
+  }
+}
